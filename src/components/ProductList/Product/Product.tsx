ab8ec0d452c6c7062880a848eeb6a065eecdaf92
Andrew <andrewpeliza@hotmail.com>
2019-11-22 19:42:52 +0000

    Basic displaying products on page, need styling.

--- src/components/ProductList/Product/Product.tsx
+++ src/components/ProductList/Product/Product.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const Product = () => <p>Product</p>;
+const Product = (props: any) => {
+  return (
+    <li className='menuItem'>
+      <div className='menuItem__info'>
+        <div className='menuItem__info--name--desc'>
+          <h1>{props.itemName}</h1>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default Product;
--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Product from './Product/Product';
 
+const theProducts = ['iphone', 'samsung', 'nokia'];
+
 const ProductList: React.FC = () => {
-  return (
-    <>
-      <Product />
-    </>
-  );
+  const products = theProducts.map(p => {
+    return <Product itemName={p} />;
+  });
+
+  return <>{products}</>;
 };
 
 export default ProductList;
